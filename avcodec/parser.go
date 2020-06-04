aef0f814b6b749d88029f22c1f88aaa66d828013
mengxiaowei1 <mengxiaowei@jd.com>
2020-06-04 20:20:45 +0800

    add height, pix_fmt, top_field

--- avcodec/parser.go
+++ avcodec/parser.go
@@ -8,3 +8,15 @@ import "C"
 func (ctxt *ParserContext) Width() int {
     return int(ctxt.width)
 }
+
+func (ctxt *ParserContext) Height() int {
+    return int(ctxt.height)
+}
+
+func (ctxt *ParserContext) PixFmt() PixelFormat {
+    return (PixelFormat)(ctxt.format)
+}
+
+func (ctxt *ParserContext) FieldOrder() AvFieldOrder {
+    return (AvFieldOrder)(ctxt.field_order)
+}
