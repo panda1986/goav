72d85d4b49f3eef33432942294b50a1fe62cece8
mengxiaowei1 <mengxiaowei@jd.com>
2020-06-04 19:41:44 +0800

    change parser from avformat to avcodecc

--- avcodec/parser.go
+++ avcodec/parser.go
@@ -1,10 +1,10 @@
-package avformat
+package avcodec
 
 //#cgo pkg-config: libavformat
 //#include <libavformat/avformat.h>
 
 import "C"
 
-func (ctxt *CodecParserContext) Width() int {
+func (ctxt *ParserContext) Width() int {
     return int(ctxt.width)
 }
